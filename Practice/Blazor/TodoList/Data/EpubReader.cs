f4118a605fc751fae70599660e0644f98ea1e4ce
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-25 20:32:37 -0400

    Learned out to manipulate Hrefs

--- Practice/Blazor/TodoList/Data/EpubReader.cs
+++ Practice/Blazor/TodoList/Data/EpubReader.cs
@@ -1,8 +1,7 @@
 namespace TodoList.Data;
 using EpubSharp;
 using AngleSharp;
-using AngleSharp.Html.Parser;
-
+using AngleSharp.Html.Dom;
 public class EBook {
     public EpubBook book { get; } = EpubReader.Read(@"C:\Users\mokay\Local-Repositories\Epub-Reader-PWA\Practice\Blazor\TodoList\Data\frankenstien.epub");
     public string hi = "Hello Ig";
@@ -12,9 +11,40 @@ public class EBook {
         using var context = BrowsingContext.New(config);
         using var doc = await context.OpenAsync(req => req.Content(book.Resources.Html.ElementAt(1).TextContent));
 
-        return doc.Body.InnerHtml.Trim();// var parser = new Parser();
+        return doc?.Body.InnerHtml.Trim();// var parser = new Parser();
         // var document = parser.Parse(book.Resources.Html);
         // string html = document.All.Where(m => m.LocalName == "body").First();
         // return html;
+    }
+
+    public async Task GetTOF(){
+        var config = Configuration.Default;
+        using var context = BrowsingContext.New(config);
+        using var doc = await context.OpenAsync(req => req.Content(book.Resources.Html.ElementAt(0).TextContent));
+        var anchorElements = doc.QuerySelectorAll("a")
+                            .OfType<IHtmlAnchorElement>()
+                            .SetHref(a => a.Href = a.Href.Split('#').Last())
+                            .ToList();
+        // .Select(a => ((IHtmlAnchorElement)m).Href)
+        // .ToList();
+        // Console.WriteLine(anchorElement.Href);
+        foreach (var a in anchorElements){
+            Console.WriteLine(a.Href , ", ");
+        }
+        // var aes = doc.All.Where(m => ((IHtmlAnchorElement)m).Href).ToList();
+        // Console.WriteLine(aes);
+        // foreach (AngleSharp.Dom.IElement ae in aes)
+        // {
+        // }
+    }
+}
+
+public static class MyExtentions{
+    public static IEnumerable<T> SetHref<T>(this IEnumerable<T> items, Action<T> updateMethod){
+        foreach (T item in items){
+            
+            updateMethod(item);
+        }
+        return items;
     }
 }
