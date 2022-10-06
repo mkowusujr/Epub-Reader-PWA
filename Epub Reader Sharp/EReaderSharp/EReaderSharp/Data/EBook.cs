6423601a64ba16d9717e28ed5be5df7ce9be97a6
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-10-06 10:38:49 -0400

    Began db setup

---
+++ Epub Reader Sharp/EReaderSharp/EReaderSharp/Data/EBook.cs
@@ -0,0 +1,10 @@
+﻿namespace EReaderSharp.Data;
+
+public class EBook
+{
+    public int id { get; set; }
+
+    public string filePath { get; set; }
+
+    public bool isMarkAsFavorite { get; set; }
+}
---
+++ Epub Reader Sharp/EReaderSharp/EReaderSharp/Data/EBookDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EReaderSharp.Data;
+
+public class EBookDbContext: DbContext
+{
+    public DbSet<EBook> EBook { get; set; }
+    public string DbPath { get; }
+
+    public EBookDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "elibrary.db");
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {;
+        base.OnModelCreating(modelBuilder);
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
