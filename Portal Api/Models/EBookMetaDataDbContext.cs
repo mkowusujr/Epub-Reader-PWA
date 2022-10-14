036da2d3cdd34a48df4e13b6381eb04900446ff9
mkowusujr <mrfriiday@gmail.com>
2022-10-13 21:43:44 -0400

    Created db context

---
+++ Portal Api/Models/EBookMetaDataDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Portal_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class EBookMetaDataDbContext: DbContext
+{
+    public DbSet<EBookMetaData> EBook { get; set; }
+
+    public EBookMetaDataDbContext(DbContextOptions<EBookMetaDataDbContext> options): base(options)
+    { }
+}
