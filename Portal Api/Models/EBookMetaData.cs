afb941b52f2bbebaa447680522a73bce716a7356
mkowusujr <mrfriiday@gmail.com>
2022-10-13 21:22:32 -0400

    Created database item

---
+++ Portal Api/Models/EBookMetaData.cs
@@ -0,0 +1,10 @@
+﻿namespace Portal_Api.Models;
+
+public class EBookMetaData
+{
+    public int Id { get; set; }
+
+    public string FilePath { get; set; }
+
+    public bool IsMarkAsFavorite { get; set; }
+}
